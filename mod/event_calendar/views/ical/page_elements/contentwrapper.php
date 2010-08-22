80fe454631bd9f79399ed9f0279350b1abda275c
Klaas Freitag <freitag@suse.de>
2010-08-22 15:52:20 +0200

    Files to export ical files from the event_calendar plugin

---
+++ mod/event_calendar/views/ical/page_elements/contentwrapper.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *
+ * @package Elgg
+ * @subpackage Core
+ * @author Curverider Ltd
+ * @link http://elgg.org/
+ *
+ * @uses $vars['value'] The URL to display
+ *
+ */
+	echo $vars['body'];
+?>
+
