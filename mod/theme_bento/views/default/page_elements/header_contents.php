487ebd8a39055fdad38ed9252541c34f1a601d5e
Thomas Schmidt <tschmidt@suse.de>
2010-08-27 16:50:42 +0200

    add the bento javascript login box

--- mod/theme_bento/views/default/page_elements/header_contents.php
+++ mod/theme_bento/views/default/page_elements/header_contents.php
@@ -14,6 +14,7 @@
 <div id="subheader" class="container_16">
     <div id="breadcrump" class="grid_9 alpha">
         <a href="/"><img src="<?php echo $vars['url']; ?>mod/theme_bento/graphics/home_grey.png" alt="Connect Home"/></a>
+        <a href="/">Connect Home</a>
     </div>
 
     <div id="local-user-actions" class="grid_7 omega textright">
@@ -48,8 +49,30 @@
         <!-- Link: Logout -->
         <?php echo elgg_view('output/url', array('href' => "{$vars['url']}action/logout", 'text' => elgg_echo('logout'), 'is_action' => TRUE)); ?>
 
-      <?php } ?> 
-      
+      <?php } else { ?>
+
+        <a href="https://<?php echo $_SERVER['SERVER_NAME'] ?>/ICSLogin/?%22<?php echo $_SERVER['SERVER_NAME'] . '/' . $_SERVER['REQUEST_URI'] ?>%22">Sign up</a>| <a href="#" id="login-trigger">Login</a>
+
+      <div id="login-form">
+        <form action="https://<?php echo $_SERVER['SERVER_NAME'] ?>/ICSLogin/auth-up"
+              method="post" enctype="application/x-www-form-urlencoded" id="login_form">
+          <div class="hidden">
+            <input name="url" value="http://<?php echo $_SERVER['SERVER_NAME'] . '/' . $_SERVER['REQUEST_URI'] ?>" type="hidden" />
+            <input name="context" value="default" type="hidden" />
+            <input name="proxypath" value="reverse" type="hidden" />
+            <input name="message" value="Please log In" type="hidden" />
+            <input name="return_to_path" value="<%= @return_to_path %>" type="hidden" />
+          </div>
+          <p><label class="inlined" for="username">Username</label><input type="text" class="inline-text" name="username" value="" id="username" /></p>
+          <p><label class="inlined" for="password">Password</label><input type="password" class="inline-text" name="password" value="" id="password" /></p>
+
+          <p><input value="Login" type="submit" onclick="fillEmptyFields();" /></p>
+
+          <p class="slim-footer"><a id="close-login" href="#">Cancel</a></p>
+        </form>
+      </div>
+
+      <?php } ?>
     </div>
 </div>
 
