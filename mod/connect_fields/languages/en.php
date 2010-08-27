b8105494538304e05aaa684aedae6540f8a06647
Pavol Rusnak <stick@gk2.sk>
2010-08-27 14:10:18 +0200

    add new fields to profile/group

--- mod/connect_fields/languages/en.php
+++ mod/connect_fields/languages/en.php
@@ -1,16 +1,44 @@
 <?php
 
   $english = array(
+
       'checkbox:yes'             => 'Yes',
       'checkbox:no'              => 'No',
       'checkbox:empty'           => 'Not set',
-      'profile:beer'             => 'Do you like beer?',
-      'profile:country'          => 'Country you live in',
+
+      'profile:blog'             => 'Blog',
+      'profile:country'          => 'Current country',
+      'profile:xmpp'             => 'Jabber (XMPP)',
+      'profile:freenode_nick'    => 'Freenode IRC nick',
+      'profile:freenode_cloak'   => 'Freenode IRC cloak',
+      'profile:email_alias'      => 'opensuse.org email alias',
+      'profile:openid'           => 'OpenID',
+      'profile:gpg'              => 'GPG key',
+      'profile:ssh'              => 'SSH key',
+      'profile:twitter'          => 'Twitter',
+      'profile:identica'         => 'identi.ca',
+      'profile:skype'            => 'Skype',
+      'profile:faceboook'        => 'Facebook',
+      'profile:ohloh'            => 'ohloh',
+      'profile:gitorious'        => 'Gitorious',
+      'profile:github'           => 'GitHub',
+      'profile:linkedin'         => 'LinkedIn',
+      'profile:xing'             => 'Xing',
+
+      'groups:blog'             => 'Blog',
+      'groups:irc'              => 'IRC channel',
+      'groups:twitter'          => 'Twitter',
+      'groups:identica'         => 'identi.ca',
+      'groups:faceboook'        => 'Facebook',
+      'groups:obs'              => 'Build Service repository',
+      'groups:ohloh'            => 'ohloh',
+      'groups:bugtracker'       => 'Upstream bugtracker',
+
       'profile:data:country'     => 'cz:de:sk',
       'profile:data:country:cz'  => 'Czech Republic',
       'profile:data:country:sk'  => 'Slovakia',
       'profile:data:country:de'  => 'Germany',
-        );
+  );
 
   add_translation("en",$english);
 
--- mod/connect_fields/start.php
+++ mod/connect_fields/start.php
@@ -1,10 +1,42 @@
 <?php
-    function connect_fields_setup($hook_name, $entity_type, $return_value, $parameters) {
+    function connect_fields_profile_setup($hook_name, $entity_type, $return_value, $parameters) {
         global $CONFIG;
-        // add fields here
-        $return_value["beer"]="profile_checkbox";
-        $return_value["country"]="profile_dropdown";
+        $return_value["blog"] = "url";
+        $return_value["country"] = "profile_dropdown";
+        $return_value["xmpp"] = "text";
+        $return_value["freenode_nick"] = "text";
+        $return_value["freenode_cloak"] = "text";
+        $return_value["email_alias"] = "text";
+        $return_value["openid"] = "text";
+        $return_value["gpg"] = "text";
+        $return_value["ssh"] = "longtext";
+        $return_value["twitter"] = "url";
+        $return_value["identica"] = "url";
+        $return_value["skype"] = "text";
+        $return_value["faceboook"] = "url";
+        $return_value["ohloh"] = "url";
+        $return_value["gitorious"] = "url";
+        $return_value["github"] = "url";
+        $return_value["linkedin"] = "url";
+        $return_value["xing"] = "url";
+        unset($return_value["skills"]);
+        unset($return_value["interests"]);
         return $return_value;
     }
-    register_plugin_hook('profile:fields', 'profile', 'connect_fields_setup');
+
+    function connect_fields_group_setup($hook_name, $entity_type, $return_value, $parameters) {
+        global $CONFIG;
+        $return_value["blog"] = "url";
+        $return_value["irc"] = "text";
+        $return_value["twitter"] = "url";
+        $return_value["identica"] = "url";
+        $return_value["faceboook"] = "url";
+        $return_value["obs"] = "text";
+        $return_value["ohloh"] = "url";
+        $return_value["bugtracker"] = "url";
+        return $return_value;
+    }
+
+    register_plugin_hook('profile:fields', 'profile', 'connect_fields_profile_setup');
+    register_plugin_hook('profile:fields', 'group',   'connect_fields_group_setup');
 ?>
