a9ecf1430d4f8350a96b673dca0056cf71fd50ad
Robert Lihm <rlihm@suse.de>
2010-09-10 13:30:56 +0200

    changed spacing of plugin-headline and add style for plugin details

--- mod/theme_bento/views/default/css.php
+++ mod/theme_bento/views/default/css.php
@@ -30,11 +30,6 @@
     border-color: #768d99;
     border-color: #80A0B2;
     background-color: #f6f6f6;
-    
-/*    box-shadow: 0 1px 3px #999;
-    -moz-box-shadow: 0 1px 3px #999;
-    -webkit-box-shadow: 0 1px 3px #999;*/
-    
   }
 
 .plugin_details.not-active {
@@ -43,10 +38,18 @@
 }
   .plugin_details h3 {
     font-size: 1.5em;
+    margin-top: 0.5em !important;
+    text-shadow: #999 0 1px 1px;
   }
 
 .plugin_details a.button {
   border: 1px solid #c9c9c9;
+}
+
+.manifest_file {
+  margin: 0 1em 1em 1em;
+  border-top: 1px dotted #ccc;
+  padding-top: 0.5em;
 }
 
 .admin_plugin_enable_disable,
