e6fd6ae0c8d81a344f5e0e9e20c3dfcbde2addb4
Robert Lihm <rlihm@suse.de>
2010-10-07 11:57:06 +0200

    added style for member search

--- mod/theme_bento/views/default/members/search.php
+++ mod/theme_bento/views/default/members/search.php
@@ -9,16 +9,16 @@ $name_string = elgg_echo('members:search:name');
 
 ?>
 
-<div class="sidebarBox">
+<div class="sidebarBox box box-shadow navigation alpha">
 
-<h3><?php echo elgg_echo('members:searchtag'); ?></h3>
+<h2 class="box-header"><?php echo elgg_echo('members:searchtag'); ?></h2>
 <form id="memberssearchform" action="<?php echo $vars['url']; ?>mod/members/index.php?" method="get">
 	<input type="text" name="tag" value="<?php echo $tag_string; ?>" onclick="if (this.value=='<?php echo $tag_string; ?>') { this.value='' }" class="search_input" />
 	<input type="hidden" name="filter" value="search_tags" />	
 	<input type="submit" value="<?php echo elgg_echo('go'); ?>" />
 </form>
 
-<h3><?php echo elgg_echo('members:searchname'); ?></h3>
+<h2 class="box-subheader"><?php echo elgg_echo('members:searchname'); ?></h2>
 <form id="memberssearchform" action="<?php echo $vars['url']; ?>mod/members/index.php?" method="get">
 	<input type="text" name="tag" value="<?php echo $name_string; ?>" onclick="if (this.value=='<?php echo $name_string; ?>') { this.value='' }" class="search_input" />
 	<input type="hidden" name="subtype" value="" />
