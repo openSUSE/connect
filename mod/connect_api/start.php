13ed8d7caa338fe219b76d1738a33557209777a3
Michal Hrusecky <Michal@Hrusecky.net>
2010-10-07 10:06:07 +0200

    Updating API
    Better handling of errors, adding helper function to get identity of the
    caller and everything is now nice and clean. And we can probably add
    some restrictions based on key names...

--- mod/connect_api/start.php
+++ mod/connect_api/start.php
@@ -2,34 +2,97 @@
 
    $non_public = true;
 
-   function my_get_user_attr($login,$attr) {
+   // Tries to find your key in detebase and get description for it
+   function get_key_description() {
+      // Get the public key from the request
+      if(!($pub_key = get_input('api_key')))
+         return NULL;
+      // Bypass acces controls to get all the keys
+      $ia = elgg_set_ignore_access(TRUE);
+      $keys = get_entities('object', 'api_key');
+      elgg_set_ignore_access($ia);
+      // Try to find a key
+      foreach($keys as $key) {
+         if($key->public == $pub_key)
+       return $key->title;
+      }
+      // If not found, return NULL
+      return NULL;
+   }
+
+   // Returns value of the attribute
+   function connect_entity_get_attribute($guid,$attr) {
+      // Get key description (we might be interested in who is asking)
+      $key = get_key_description();
+
+      // If there is no such entity, return NULL
+      if (!($user = get_entity($guid)))
+         return NULL;
+
+      // Test whether it is attribute of the object itself
+      if ($result = $user->$attr)
+         return array($attr => $result);
+
+      // Test whether it is among metadata
+      // Bypass access controls if we are authenticated
+      if($key)
+         $ia = elgg_set_ignore_access(TRUE);
+      $result = get_metadata_byname($user->guid, $attr);
+      // Restore access controls
+      if($key)
+         elgg_set_ignore_access($ia);
+      // If we found anything, return that
+      if($result)
+         return array($attr => $result->value);
+
+      // We want all metadata?
+      if ($attr == '*' ) {
+         // Bypass access controls if we are authenticated
+         if($key)
+            $ia = elgg_set_ignore_access(TRUE);
+         $md = get_metadata_for_entity($user->guid);
+         // Restore access control
+         if($key)
+            elgg_set_ignore_access($ia);
+
+         $result = array();
+         foreach($md as $m) {
+            if($m->owner_guid == $user->guid)
+               $result = $result + array($m->name => $m->value);
+         }
+         return array($attr => $result);
+      }
+
+      return  array($attr => NULL);
+   }
+
+   // Wrapper to allow to get attributes for login
+   function connect_user_get_attribute($login,$attr) {
       if (($user = get_user_by_username($login)) &&
             (!($user->isBanned()))) {
-         return array($attr => $user->$attr);
+         return  connect_entity_get_attribute($user->guid,$attr);
       }
-      return false;
+      return NULL;
    }
 
-   function my_get_ent_attr($guid,$attr) {
-      if ($group = get_entity($guid)) {
-         return array($attr => $group->$attr);
-      }
-      return false;
-   }
-
-   function my_get_user_groups($login) {
+   // Returns list of users groups
+   function connect_user_get_groups($login) {
    if (($user = get_user_by_username($login)) && (!($user->isBanned()))) {
+         // Formulate what we want
          $options = array(
                   'relationship' => 'member',
-                  'relationship_guid' => $user->guid, 
-                  'inverse_relationship' => FALSE, 
-                  'types' => 'group', 
-                  'subtypes' => '', 
-                  'owner_guid' => 0, 
+                  'relationship_guid' => $user->guid,
+                  'inverse_relationship' => FALSE,
+                  'types' => 'group',
+                  'subtypes' => '',
+                  'owner_guid' => 0,
          );
+         // Get all groups with desired properties
          $groups = elgg_get_entities_from_relationship($options);
+         // Formulate the response
          $grp_data = array();
          foreach($groups as $group) {
+            // Return owner of that group as well
             if($owner = get_user($group->owner_guid)) {
                $owner = $owner->username;
             } else {
@@ -39,7 +102,7 @@
                                              "guid" => $group->guid,
                                              "name" => $group->name,
                                              "owner" => $owner,
-                                             "public" => 
+                                             "public" =>
                                                 ($group->membership == ACCESS_PUBLIC)
                                              ));
          }
@@ -47,11 +110,13 @@
                        "groups" => $grp_data
                      );
       }
-      return false;
+      return NULL;
     }
 
-    expose_function("connect.user.get.groups", 
-                "my_get_user_groups", 
+    // Register our API
+
+    expose_function("connect.user.get.groups",
+                "connect_user_get_groups",
                  array("login" => array(
                            'type' => 'string',
                            'required' => true)),
@@ -61,8 +126,8 @@
                  false
                 );
 
-    expose_function("connect.user.get.attribute", 
-                "my_get_user_attr", 
+    expose_function("connect.user.get.attribute",
+                "connect_user_get_attribute",
                  array("login" => array(
                            'type' => 'string',
                            'required' => true),
@@ -76,8 +141,8 @@
                  false
                 );
 
-    expose_function("connect.entity.get.attribute", 
-                "my_get_ent_attr", 
+    expose_function("connect.entity.get.attribute",
+                "connect_entity_get_attribute",
                  array("guid" => array(
                            'type' => 'int',
                            'required' => true),
@@ -91,4 +156,12 @@
                  false
                 );
 
+    expose_function("connect.whoami",
+                "get_key_description",
+                 array(),
+                 'Returns description of your key',
+                 'GET',
+                 true,
+                 false
+                );
 ?>
