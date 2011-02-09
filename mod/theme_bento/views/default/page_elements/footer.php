9546c49689078eb51ec4fc52fdcfca04e9803f20
Thomas Schmidt <tschmidt@suse.de>
2011-02-09 16:49:57 +0100

    add piwik code

--- mod/theme_bento/views/default/page_elements/footer.php
+++ mod/theme_bento/views/default/page_elements/footer.php
@@ -68,8 +68,20 @@
 
 
 <!-- insert an analytics view to be extended -->
-<?php
-	echo elgg_view('footer/analytics');
-?>
+
+<!-- Piwik -->
+<script type="text/javascript">
+    var pkBaseURL = (("https:" == document.location.protocol) ? "https://features.opensuse.org/piwik/" : "http://features.opensuse.org/piwik/");
+    document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
+</script><script type="text/javascript">
+    try {
+        var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 4);
+        piwikTracker.trackPageView();
+        piwikTracker.enableLinkTracking();
+    } catch( err ) {}
+</script><noscript><p><img src="https://features.opensuse.org/piwik/piwik.php?idsite=4" style="border:0" alt="" /></p></noscript>
+<!-- End Piwik Tracking Tag -->
+
+
 </body>
 </html>
