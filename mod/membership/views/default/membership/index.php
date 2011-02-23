4702f0193e945311b2c90f4fb4f39cf9cd2a501b
Thomas Schmidt <tschmidt@suse.de>
2011-02-23 15:32:13 +0100

    add membership widget

--- mod/membership/views/default/membership/index.php
+++ mod/membership/views/default/membership/index.php
@@ -4,4 +4,6 @@
 		
 ?>
 
-test
+<p>
+To be done. From retro: a page where you can read/sign the GP’s and apply for membership
+</p>
--- mod/membership/views/default/widgets/membership/view.php
+++ mod/membership/views/default/widgets/membership/view.php
@@ -1,5 +1,4 @@
 <div class="contentWrapper">
-
-    Blank Membership widget
-
+    <img src="<?php echo $vars['url']; ?>mod/membership/graphics/members.jpg" alt="Board"/><br/>
+    Contribute to openSUSE and become an <a href="http://en.opensuse.org/openSUSE:Members">openSUSE member</a>!
 </div>
