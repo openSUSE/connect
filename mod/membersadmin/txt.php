3f6166335a651d3e108c1311277c5851bed497db
Pavol Rusnak <stick@gk2.sk>
2011-02-24 18:10:22 +0100

    add missing newlines

--- mod/membersadmin/txt.php
+++ mod/membersadmin/txt.php
@@ -8,7 +8,7 @@
   header('Content-Disposition: attachment; filename="opensuse_cloaks.txt"');
 
   foreach ($members as $m) {
-      echo "{$m->freenode_nick}\t{$m->freenode_cloak}";
+      echo "{$m->freenode_nick}\t{$m->freenode_cloak}\n";
   }
 
 ?>
