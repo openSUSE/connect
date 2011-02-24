60b1cb84cc34842fa0e7707a8dbf89d4ecddb4db
Pavol Rusnak <stick@gk2.sk>
2011-02-24 17:21:24 +0100

    add missing ;

--- mod/membersadmin/views/default/membersadmin/list.php
+++ mod/membersadmin/views/default/membersadmin/list.php
@@ -22,7 +22,7 @@
   echo "<pre>\n";
   echo "username;email_target;email_nick;email_full;freenode_nick;freenode_cloak;cloak_applied\n";
   foreach ($members as $m) {
-      echo "{$m->username};{$m->email_target};{$m->email_nick};{$m->email_full};{$m->freenode_nick};{$m->freenode_cloak}";
+      echo "{$m->username};{$m->email_target};{$m->email_nick};{$m->email_full};{$m->freenode_nick};{$m->freenode_cloak};";
       echo $m->cloak_applied ? "1\n" : "0\n";
   }
   echo "</pre>\n";
