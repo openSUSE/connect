d4e6c292c2101d127f02be5bd0d60ad1056eb94b
Pavol Rusnak <stick@gk2.sk>
2011-02-24 14:50:05 +0100

    generate simple CSV with members info

--- mod/membersadmin/views/default/membersadmin/list.php
+++ mod/membersadmin/views/default/membersadmin/list.php
@@ -1,5 +1,16 @@
 <?php
 
-  echo 'here i am';
+  define('MEMBERSID', 111);
+
+  $membersgroup = new ElggGroup(MEMBERSID);
+
+  $members = $membersgroup->getMembers();
+
+  echo "<pre>\n";
+  echo "username;email_target;email_nick;email_full;freenode_nick;freenode_cloak\n";
+  foreach ($members as $m) {
+      echo "{$m->username};{$m->email_target};{$m->email_nick};{$m->email_full};{$m->freenode_nick};{$m->freenode_cloak}\n";
+  }
+  echo "</pre>\n";
 
 ?>
