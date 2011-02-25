cf0cff0ec2f0e9d6870a2a13e6685262a294e2d4
Pavol Rusnak <stick@gk2.sk>
2011-02-25 16:25:35 +0100

    put input/output elements back (accidentaly removed by update)

---
+++ views/default/output/profile_checkbox.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ *
+ * @uses $vars['value'] Whether the checkbox is set
+ *
+ */
+
+if ($vars['value']>0) {
+	echo "Yes";
+} else {
+        echo "No";
+}
---
+++ views/default/output/profile_dropdown.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ *
+ * @uses $vars['value'] Whether the checkbox is set
+ *
+ */
+
+echo elgg_echo( "profile:data:" . $vars['value'] );
