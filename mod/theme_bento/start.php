535ea6377cbda2c76fe94fd5d7f19db1b4d07d65
Thomas Schmidt <tschmidt@suse.de>
2011-05-06 14:11:14 +0200

    fix protocol when running with new fake-ichain

--- mod/theme_bento/start.php
+++ mod/theme_bento/start.php
@@ -17,6 +17,15 @@ function bento_init() {
     $CONFIG->bento_path = "https://static.opensuse.org/themes/bento";
     // $CONFIG->bento_path = "/themes/bento";
 
+    // setting correct base url
+    $pathpart = str_replace("//","/",str_replace($_SERVER['DOCUMENT_ROOT'],"",$CONFIG->path));
+    if (substr($pathpart,0,1) != "/") {
+        $pathpart = "/" . $pathpart;
+    }
+    $protocol = "http";
+    if (!empty($_SERVER['HTTP_X_FORWARDED_PROTO'])) $protocol = $_SERVER['HTTP_X_FORWARDED_PROTO'];
+    $CONFIG->url = $protocol . "://" . $_SERVER['HTTP_HOST'] . $pathpart;
+
 }
 
 // Initialise log browser
