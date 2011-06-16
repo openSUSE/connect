a2fd81ba790028978d99ad632cfc9bdbcd5bbb85
Robert Lihm <rlihm@suse.de>
2011-06-16 14:14:15 +0200

    * added prusnaks snippet to membershipreq.php * added 1st visual changes for member-request page

--- mod/groups/views/default/groups/membershiprequests.php
+++ mod/groups/views/default/groups/membershiprequests.php
@@ -9,24 +9,25 @@
 ?>
 	<div class="reportedcontent_content active_report">
 		<div class="groups_membershiprequest_buttons">
-			<div style="float: left; width: 20%;">
+			<div class="groupe-user grid_5">
 			<?php
+			  // Add User Icon
 				echo "<div class=\"member_icon\"><a href=\"" . $request->getURL() . "\">";
 				echo elgg_view("profile/icon", array(
 					'entity' => $request,
 					'size' => 'small',
 					'override' => 'true'
 				));
-				echo "</a></div>{$request->name}<br />";
+				echo "</a></div>{$request->name}<br />"; // Add User Name
 
-				echo str_replace('<a', '<a class="delete_report_button" ', elgg_view('output/confirmlink',array(
+				echo str_replace('<a', '<a class="delete_report_button red" ', elgg_view('output/confirmlink',array(
 					'href' => $vars['url'] . 'action/groups/killrequest?user_guid='.$request->guid.'&group_guid=' . $vars['entity']->guid,
 					'confirm' => elgg_echo('groups:joinrequest:remove:check'),
 					'text' => elgg_echo('groups:joinrequestdecline'),
 				)));
 			$url = elgg_add_action_tokens_to_url("{$vars['url']}action/groups/addtogroup?user_guid={$request->guid}&group_guid={$vars['entity']->guid}");
 			?>
-			<a href="<?php echo $url; ?>" class="archive_report_button"><?php echo elgg_echo('groups:joinrequestaccept'); ?></a>
+			<strong><a href="<?php echo $url; ?>" class="archive_report_button green"><?php echo elgg_echo('groups:joinrequestaccept'); ?></a></strong>
 			</div>
 			<?php if ($vars['extended']) {
 				$thumburl = elgg_add_action_tokens_to_url("{$vars['url']}action/groups/thumbvote?user_guid={$request->guid}&group_guid={$vars['entity']->guid}");
