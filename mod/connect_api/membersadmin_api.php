df890238adb4a567e7e58f74475d7fd96fe2b61b
Pavol Rusnak <stick@gk2.sk>
2011-06-29 17:30:17 +0200

    introduce connect.membersadmin.membermails API call for simple list of members' emails

--- mod/connect_api/membersadmin_api.php
+++ mod/connect_api/membersadmin_api.php
@@ -2,7 +2,6 @@
 
 require_once(dirname(dirname(__FILE__)) . '/membersadmin/maildump_func.php');
 
-
 function connect_membersadmin_maildump() {
     $key = get_key_description();
     if ($key != 'members_admin') {
@@ -13,16 +12,29 @@ function connect_membersadmin_maildump() {
     connect_maildump_func();
 }
 
-// Expose our api methods
-// expose_function ($method, $function, array $parameters=NULL,
-//                  $description="", $call_method="GET", $require_api_auth=false,
-//                  $require_user_auth=false)
+function connect_membersadmin_membermails() {
+    $key = get_key_description();
+    if ($key != 'members_admin') {
+        return new ErrorResult("Your used key doesn't have sufficient permissions for members administration!");
+    }
 
+    header('Content-type: text/plain; charset=utf-8');
+    connect_maildump_func(false);
+}
 
 expose_function("connect.membersadmin.maildump",
         "connect_membersadmin_maildump",
         array(),
         'Returns the members maildump data',
+        'GET',
+        $require_api_auth,
+        false
+);
+
+expose_function("connect.membersadmin.membermails",
+        "connect_membersadmin_membermails",
+        array(),
+        'Returns the list of opensuse members emails',
         'GET',
         $require_api_auth,
         false
--- mod/membersadmin/maildump_func.php
+++ mod/membersadmin/maildump_func.php
@@ -12,7 +12,7 @@ function subval_sort($a,$subkey) {
 }
 
 // separate method, used in connect_api module as well
-function connect_maildump_func() {
+function connect_maildump_func($full = true) {
     $membersgroup = new ElggGroup($CONFIG->MembersGroupID);
     $members = $membersgroup->getMembers(0);
     $members = array_unique($members, SORT_REGULAR);
@@ -20,14 +20,20 @@ function connect_maildump_func() {
 
     foreach ($members as $m) {
         if (!$m->email_target) continue;
-        if ($m->email_nick) {
-            printf("%-40s %s\n", $m->email_nick, $m->email_target);
-        }
-        if ($m->email_full) {
-            printf("%-40s %s\n", $m->email_full, $m->email_target);
+        if ($full) {
+            if ($m->email_nick) {
+                printf("%-40s %s\n", $m->email_nick, $m->email_target);
+            }
+            if ($m->email_full) {
+                printf("%-40s %s\n", $m->email_full, $m->email_target);
+            }
+        } else {
+            printf("%s\n", $m->email_nick);
         }
     }
-    printf("%-40s %s\n", 'gnokii@opensuse.org', 'board@opensuse.org');
+    if ($full) {
+        printf("%-40s %s\n", 'gnokii@opensuse.org', 'board@opensuse.org');
+    }
 }
 
 ?>
