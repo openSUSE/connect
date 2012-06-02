5f12248c3874d2da02f15d38e73b4697d9e17539
Athanasios-Ilias Rousinopoulos <athanrous@gmail.com>
2012-06-02 03:16:15 +0300

    Rename the $event variable to $ed.

--- mod/event_calendar/models/model.php
+++ mod/event_calendar/models/model.php
@@ -243,7 +243,7 @@ function event_calendar_set_event_from_form() {
 		$result->success = false;
 	}
 	
-	$test = $event->material;
+	$test = $ed->material;
 	$test->annotate('comment', $comment_text, $test->access_id);
 	return $result;
 }
