6dac22e9ca5df24f27983c2f43bf0271b0b19ec0
Athanasios-Ilias Rousinopoulos <athanrous@gmail.com>
2012-06-04 16:14:23 +0300

    Annotate the $event , and assign a value $annotations in order to get and print the annotations.

--- mod/event_calendar/models/model.php
+++ mod/event_calendar/models/model.php
@@ -206,7 +206,9 @@ function event_calendar_set_event_from_form() {
 		$event->travel=$ed->travel;
 		$event->booth=$ed->booth;
 		$event->real_end_time = event_calendar_get_end_time($event);
-		
+		$event->annotate('comment', $comment_text, $event->access_id);
+		$annotations = $event->getAnnotations('comment',10,1);
+		$body .= elgg_echo($annotations);
 		$result->success = $event->save();
 		if ($result->success) {
 			if ($group_guid && (get_plugin_setting('autogroup', 'event_calendar') == 'yes')) {
