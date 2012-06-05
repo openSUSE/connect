5e6ad3d64a01fc560aa63bfc2d604e76c6d21bf3
Athanasios-Ilias Rousinopoulos <athanrous@gmail.com>
2012-06-05 18:17:09 +0300

    Re-try to print them.

--- mod/event_calendar/views/default/object/event_calendar.php
+++ mod/event_calendar/views/default/object/event_calendar.php
@@ -20,21 +20,25 @@ if ($vars['full']) {
 	$body .= '<div class="contentWrapper" >';
 	
 	$material_annotation = $event->getAnnotations('material_comment');
-	print_r ( $material_annotation[0][value]);
-	
 	$arrival_annotation = $event->getAnnotations('arrival_comment');
-	print_r ( $arrival_annotation[0][value]);
-	
-	
 	$departure_annotation = $event->getAnnotations('departure_comment');
-	print_r ( $departure_annotation[0][value]);
-	
 	$talks_annotation = $event->getAnnotations('talks_comment');
-	print_r ( $talks_annotation[0][value]);
+	$booth_annotation = $event->getAnnotations('booth_comment');
+	print ( $material_annotation[0][value]);
+	
+	//$arrival_annotation = $event->getAnnotations('arrival_comment');
+	print ( $arrival_annotation[0][value]);
+	
+	
+	//$departure_annotation = $event->getAnnotations('departure_comment');
+	print ( $departure_annotation[0][value]);
+	
+	//$talks_annotation = $event->getAnnotations('talks_comment');
+	print ( $talks_annotation[0][value]);
 	
 
-	$booth_annotation = $event->getAnnotations('booth_comment');
-	print_r ( $booth_annotation[0][value]);
+	//$booth_annotation = $event->getAnnotations('booth_comment');
+	print ( $booth_annotation[0][value]);
 	
 	foreach($event_items as $item) {
 		$value = $item->value;
