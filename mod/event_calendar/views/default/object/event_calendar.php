91c1f88cade914dac442d4f29fb13c6b9b3a62a6
Athanasios-Ilias Rousinopoulos <athanrous@gmail.com>
2012-06-21 18:04:45 +0300

    Comment the last php code.

--- mod/event_calendar/views/default/object/event_calendar.php
+++ mod/event_calendar/views/default/object/event_calendar.php
@@ -56,62 +56,62 @@ $form_body = <<<EOT
 EOT;
 				
 ?>
-
 <?php 
-if ($vars['full']) {
-	$body = elgg_view('event_calendar/strapline',$vars);
-	$event_items = event_calendar_get_formatted_full_items($event);
+// 
+//if ($vars['full']) {
+//	$body = elgg_view('event_calendar/strapline',$vars);
+	//$event_items = event_calendar_get_formatted_full_items($event);
 	
-	$body .= '<div class="contentWrapper" >';
+	//$body .= '<div class="contentWrapper" >';
 	
-	foreach($event_items as $item) {
+	//foreach($event_items as $item) {
 		
-		$value = $item->value;
-		if (!empty($value)) {
-			if ($vars['full']) {
-				$even_odd = ( 'odd' != $even_odd ) ? 'odd' : 'even';
-				$body .= "<p class=\"{$even_odd}\"><b>";
-				$body .= $item->title.':</b> ';
-				$body .= $item->value;
-			}		
-		}
-	}
+	//	$value = $item->value;
+	//	if (!empty($value)) {
+		//	if ($vars['full']) {
+			//	$even_odd = ( 'odd' != $even_odd ) ? 'odd' : 'even';
+				//$body .= "<p class=\"{$even_odd}\"><b>";
+				//$body .= $item->title.':</b> ';
+				//$body .= $item->value;
+		//	}		
+		//}
+	//}
 	
-	echo $body;
-	
-	
+	//echo $body;
 	
 	
-	if ($event->long_description) {
-		echo '<p>'.$event->long_description.'</p>';
-	} else {
-		echo '<p>'.$event->description.'</p>';
-	}
-	echo '</div>';
-	if (get_plugin_setting('add_to_group_calendar', 'event_calendar') == 'yes') {
-		echo elgg_view('event_calendar/forms/add_to_group',array('event' => $event));
-	}
-} else {
-	$time_bit = event_calendar_get_formatted_time($event);
-	$icon = elgg_view(
-			"graphics/icon", array(
-			'entity' => $vars['entity'],
-			'size' => 'small',
-		  )
-		);
-	$info .= '<p><b><a href="'.$event->getUrl().'">'.$event->title.'</a></b>';
-	$info .= '<br />'.$time_bit;
-	if ($event->description) {
-		$info .= '<br /><br />'.$event->description;
-	}
 	
-	if ($event_calendar_venue_view = get_plugin_setting('venue_view', 'event_calendar') == 'yes') {
-		$info .= '<br />'.$event->venue;
-	}
-	$info .=  '</p>';
 	
-	echo elgg_view_listing($icon, $info);
-}
+	//if ($event->long_description) {
+		//echo '<p>'.$event->long_description.'</p>';
+	//} else {
+		//echo '<p>'.$event->description.'</p>';
+	//}
+	//echo '</div>';
+	//if (get_plugin_setting('add_to_group_calendar', 'event_calendar') == 'yes') {
+		//echo elgg_view('event_calendar/forms/add_to_group',array('event' => $event));
+	//}
+//} else {
+	//$time_bit = event_calendar_get_formatted_time($event);
+	//$icon = elgg_view(
+		//	"graphics/icon", array(
+			//'entity' => $vars['entity'],
+			//'size' => 'small',
+		  //)
+		//);
+	//$info .= '<p><b><a href="'.$event->getUrl().'">'.$event->title.'</a></b>';
+	//$info .= '<br />'.$time_bit;
+	//if ($event->description) {
+		//$info .= '<br /><br />'.$event->description;
+	//}
+	
+	//if ($event_calendar_venue_view = get_plugin_setting('venue_view', 'event_calendar') == 'yes') {
+		//$info .= '<br />'.$event->venue;
+	//}
+	//$info .=  '</p>';
+	
+	//echo elgg_view_listing($icon, $info);
+//}
 
 
 ?>
