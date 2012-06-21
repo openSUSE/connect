4f47654aa3a99e7195e3038e3c64f60059b7ad60
Athanasios-Ilias Rousinopoulos <athanrous@gmail.com>
2012-06-21 16:16:12 +0300

    Configure the addcomment view.

---
+++ mod/event_calendar/views/default/event_calendar/forms/addcomment.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Elgg event_calendar add comment on fields
+ *
+ * @package event_calendar
+ * 
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ * 
+ */
+
+?>
+
+	<form action="<?php echo $vars['url']; ?>event_calendar/forms/addcomment" method="post">
+		<p class="longtext_editarea">
+			<label><<br />
+			<?php
+
+				echo elgg_view("input/longtext",array(
+									"internalname" => "comment_post",
+									"value" => $body,
+													));
+			?>
+			</label>
+		</p>
+		//<p>
+		    <!-- pass across the topic guid -->
+			<input type="hidden" name="topic_guid" value="<?php echo $vars['entity']->guid; ?>" />
+			<input type="hidden" name="group_guid" value="<?php echo $vars['entity']->container_guid; ?>" />
+			
+<?php 
+		echo elgg_view('input/securitytoken');
+?>
+			<!-- display the post button -->
+			<input type="submit" class="submit_button" value="<?php echo elgg_echo('post'); ?>" />
+		</p>
+	
+	</form>
