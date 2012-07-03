7f4529c3dab746769306f6461f327ce81db9302b
Athanasios-Ilias Rousinopoulos <athanrous@gmail.com>
2012-07-03 11:55:44 +0300

    Re-try with html.

--- mod/event_calendar/views/default/object/event_calendar.php
+++ mod/event_calendar/views/default/object/event_calendar.php
@@ -218,10 +218,10 @@ if ($vars['full']) {
 		$event->annotate('travel_comment', "");
 		$travel_annotation = $event->getAnnotations('travel_comment');
 		$travel_print = $travel_annotation[0][value];
-		//$travel_body  = '<label><b>&nbsp;&nbsp;Travel Comment:</b></label><br>';
+		$travel_body  = '<label><b>&nbsp;&nbsp;Travel Comment:</b></label><br />';
 		//$travel_body .= '<br>';
-		$travel_title = "Travel Comment:";
-		$travel_body = elgg_echo("travel_title");
+		//$travel_title = "Travel Comment:";
+		//$travel_body = elgg_echo("travel_title");
 		$travel_body .= '<br>';
 		$travel_body .= elgg_view('input/longtext', array('internalname' => 'travel_comment', 'value' => $travel_print));
 		$travel_body .= '<br>';
