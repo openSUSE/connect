5b14a8885a82a069c58f974cb90d1685511f6d44
Bruno Friedmann <bruno@ioda-net.ch>
2015-01-23 09:33:30 +0100

    Add administrative method to dump members information

--- mod/connect_api/membersadmin_api.php
+++ mod/connect_api/membersadmin_api.php
@@ -24,6 +24,39 @@ function connect_membersadmin_membermails() {
     return true;
 }
 
+function connect_membersadmin_memberfullnamemail() {
+  $key = get_key_description();
+  if ($key != 'members_admin') {
+    return new ErrorResult("Your used key doesn't have sufficient permissions for members administration!");
+  }
+
+  header('Content-type: text/plain; charset=utf-8');
+  connect_membersdump_func(1);
+  return true;
+}
+
+function connect_membersadmin_memberfullnamerealmail() {
+  $key = get_key_description();
+  if ($key != 'members_admin') {
+    return new ErrorResult("Your used key doesn't have sufficient permissions for members administration!");
+  }
+
+  header('Content-type: text/plain; charset=utf-8');
+  connect_membersdump_func(2);
+  return true;
+}
+
+function connect_membersadmin_memberfulldump() {
+  $key = get_key_description();
+  if ($key != 'members_admin') {
+    return new ErrorResult("Your used key doesn't have sufficient permissions for members administration!");
+  }
+
+  header('Content-type: text/plain; charset=utf-8');
+  connect_membersdump_func(3);
+  return true;
+}
+
 expose_function("connect.membersadmin.maildump",
         "connect_membersadmin_maildump",
         array(),
@@ -42,4 +75,30 @@ expose_function("connect.membersadmin.membermails",
         false
 );
 
-?>
+expose_function("connect.membersadmin.memberfullnamemail",
+        "connect_membersadmin_memberfullnamemail",
+        array(),
+        'Returns the list of opensuse members fullname and emails',
+        'GET',
+        $require_api_auth,
+        false
+);
+
+expose_function("connect.membersadmin.memberfullnamerealmail",
+        "connect_membersadmin_memberfullnamerealmail",
+        array(),
+        'Returns the list of opensuse members fullname and real mail',
+        'GET',
+        $require_api_auth,
+        false
+);
+
+expose_function("connect.membersadmin.memberfulldump",
+        "connect_membersadmin_memberfulldump",
+        array(),
+        'Returns the list of opensuse members fullname all email (controling)',
+        'GET',
+        $require_api_auth,
+        false
+);
+
